c01a6262c136cff0be7c17180b1b40207e52c8b3
KxllSwxtch <dimashin9@gmail.com>
2025-03-19 18:15:40 +0900

    updates

--- src/pages/Catalog.jsx
+++ src/pages/Catalog.jsx
@@ -132,9 +132,7 @@ const Catalog = () => {
 		try {
 			setLoading(true)
 			const response = await axios.get(
-				`https://corsproxy.io/${encodeURIComponent(
-					`https://api.darvin.digital/api.php?method=get_cars&marka_id=${filters.brand}&model_id=${filters.model}&year_from=${filters.yearFrom}&year_to=${filters.yearTo}&mileage_from=${filters.mileageFrom}&mileage_to=${filters.mileageTo}&engine_from=${filters.capacityFrom}&engine_to=${filters.capacityTo}&price_from=${filters.priceFrom}&price_to=${filters.priceTo}&sort=${sortOption}&page=${pageNumber}`,
-				)}`,
+				`https://corsproxy.io/?url=${`https://api.darvin.digital/api.php?method=get_cars&marka_id=${filters.brand}&model_id=${filters.model}&year_from=${filters.yearFrom}&year_to=${filters.yearTo}&mileage_from=${filters.mileageFrom}&mileage_to=${filters.mileageTo}&engine_from=${filters.capacityFrom}&engine_to=${filters.capacityTo}&price_from=${filters.priceFrom}&price_to=${filters.priceTo}&sort=${sortOption}&page=${pageNumber}`}`,
 			)
 			const newCars = response.data
 
